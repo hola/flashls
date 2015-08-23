938f875fb43c0947593a2bf205a1923bc88152c7
Bahaa Aidi <bahaa.aidi@gmail.com>
2015-08-23 12:19:21 +0300

    commented-out building irrelevant stuff

--- build/build.sh
+++ build/build.sh
@@ -59,62 +59,62 @@ $FLEXPATH/bin/mxmlc ../src/org/mangui/chromeless/ChromelessPlayer.as \
 #   -target-player="11.5" \
 #   -default-size 640 480 \
 #   -default-background-color=0x000000
-
-echo "Compiling bin/release/flashlsFlowPlayer.swf"
-$FLEXPATH/bin/mxmlc ../src/org/mangui/flowplayer/HLSPluginFactory.as \
-    -source-path ../src -o ../bin/release/flashlsFlowPlayer.swf \
-    $OPT_RELEASE \
-    -library-path+=../lib/flowplayer \
-    -load-externs=../lib/flowplayer/flowplayer-classes.xml \
-    -target-player="11.5"
-./add-opt-in.py ../bin/release/flashlsFlowPlayer.swf
-
-echo "Compiling bin/debug/flashlsFlowPlayer.swf"
-$FLEXPATH/bin/mxmlc ../src/org/mangui/flowplayer/HLSPluginFactory.as \
-    -source-path ../src -o ../bin/debug/flashlsFlowPlayer.swf \
-    $OPT_DEBUG \
-    -library-path+=../lib/flowplayer \
-    -load-externs=../lib/flowplayer/flowplayer-classes.xml \
-    -target-player="11.5"
-./add-opt-in.py ../bin/debug/flashlsFlowPlayer.swf
-
-echo "Compiling bin/release/flashlsOSMF.swf"
-$FLEXPATH/bin/mxmlc ../src/org/mangui/osmf/plugins/HLSDynamicPlugin.as \
-    -source-path ../src \
-    -o ../bin/release/flashlsOSMF.swf \
-    $OPT_RELEASE \
-    -library-path+=../lib/osmf \
-    -load-externs ../lib/osmf/exclude-sources.xml \
-    -target-player="11.5" #-compiler.verbose-stacktraces=true -link-report=../test/osmf/link-report.xml
-./add-opt-in.py ../bin/release/flashlsOSMF.swf
-
-echo "Compiling bin/debug/flashlsOSMF.swf"
-$FLEXPATH/bin/mxmlc ../src/org/mangui/osmf/plugins/HLSDynamicPlugin.as \
-    -source-path ../src \
-    -o ../bin/debug/flashlsOSMF.swf \
-    $OPT_DEBUG \
-    -library-path+=../lib/osmf \
-    -load-externs ../lib/osmf/exclude-sources.xml \
-    -target-player="11.5" #-compiler.verbose-stacktraces=true -link-report=../test/osmf/link-report.xml
-./add-opt-in.py ../bin/debug/flashlsOSMF.swf
-
-echo "Compiling bin/release/flashlsOSMF.swc"
-$FLEXPATH/bin/compc -include-sources ../src/org/mangui/osmf \
-    -output ../bin/release/flashlsOSMF.swc \
-    $OPT_RELEASE \
-    -library-path+=../bin/release/flashls.swc \
-    -library-path+=../lib/osmf \
-    -target-player="11.5" \
-    -debug=false \
-    -external-library-path+=../lib/osmf
-
-echo "Compiling bin/debug/flashlsOSMF.swc"
-$FLEXPATH/bin/compc -include-sources ../src/org/mangui/osmf \
-    -output ../bin/debug/flashlsOSMF.swc \
-    $OPT_DEBUG \
-    -library-path+=../bin/debug/flashls.swc \
-    -library-path+=../lib/osmf \
-    -target-player="11.5" \
-    -debug=false \
-    -external-library-path+=../lib/osmf
-
+#
+#echo "Compiling bin/release/flashlsFlowPlayer.swf"
+#$FLEXPATH/bin/mxmlc ../src/org/mangui/flowplayer/HLSPluginFactory.as \
+#    -source-path ../src -o ../bin/release/flashlsFlowPlayer.swf \
+#    $OPT_RELEASE \
+#    -library-path+=../lib/flowplayer \
+#    -load-externs=../lib/flowplayer/flowplayer-classes.xml \
+#    -target-player="11.5"
+#./add-opt-in.py ../bin/release/flashlsFlowPlayer.swf
+#
+#echo "Compiling bin/debug/flashlsFlowPlayer.swf"
+#$FLEXPATH/bin/mxmlc ../src/org/mangui/flowplayer/HLSPluginFactory.as \
+#    -source-path ../src -o ../bin/debug/flashlsFlowPlayer.swf \
+#    $OPT_DEBUG \
+#    -library-path+=../lib/flowplayer \
+#    -load-externs=../lib/flowplayer/flowplayer-classes.xml \
+#    -target-player="11.5"
+#./add-opt-in.py ../bin/debug/flashlsFlowPlayer.swf
+#
+#echo "Compiling bin/release/flashlsOSMF.swf"
+#$FLEXPATH/bin/mxmlc ../src/org/mangui/osmf/plugins/HLSDynamicPlugin.as \
+#    -source-path ../src \
+#    -o ../bin/release/flashlsOSMF.swf \
+#    $OPT_RELEASE \
+#    -library-path+=../lib/osmf \
+#    -load-externs ../lib/osmf/exclude-sources.xml \
+#    -target-player="11.5" #-compiler.verbose-stacktraces=true -link-report=../test/osmf/link-report.xml
+#./add-opt-in.py ../bin/release/flashlsOSMF.swf
+#
+#echo "Compiling bin/debug/flashlsOSMF.swf"
+#$FLEXPATH/bin/mxmlc ../src/org/mangui/osmf/plugins/HLSDynamicPlugin.as \
+#    -source-path ../src \
+#    -o ../bin/debug/flashlsOSMF.swf \
+#    $OPT_DEBUG \
+#    -library-path+=../lib/osmf \
+#    -load-externs ../lib/osmf/exclude-sources.xml \
+#    -target-player="11.5" #-compiler.verbose-stacktraces=true -link-report=../test/osmf/link-report.xml
+#./add-opt-in.py ../bin/debug/flashlsOSMF.swf
+#
+#echo "Compiling bin/release/flashlsOSMF.swc"
+#$FLEXPATH/bin/compc -include-sources ../src/org/mangui/osmf \
+#    -output ../bin/release/flashlsOSMF.swc \
+#    $OPT_RELEASE \
+#    -library-path+=../bin/release/flashls.swc \
+#    -library-path+=../lib/osmf \
+#    -target-player="11.5" \
+#    -debug=false \
+#    -external-library-path+=../lib/osmf
+#
+#echo "Compiling bin/debug/flashlsOSMF.swc"
+#$FLEXPATH/bin/compc -include-sources ../src/org/mangui/osmf \
+#    -output ../bin/debug/flashlsOSMF.swc \
+#    $OPT_DEBUG \
+#    -library-path+=../bin/debug/flashls.swc \
+#    -library-path+=../lib/osmf \
+#    -target-player="11.5" \
+#    -debug=false \
+#    -external-library-path+=../lib/osmf
+#
